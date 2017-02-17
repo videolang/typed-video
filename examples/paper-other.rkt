e6904e75ecca35dadb1e4fdc6dabcb5bf69b3441
Stephen Chang <stchang@ccs.neu.edu>
2017-02-17 15:29:59 -0500

    port remaining racket-video tests

--- examples/paper-other.rkt
+++ examples/paper-other.rkt
@@ -1,4 +1,19 @@
-#lang video
+#lang typed/video
+(require turnstile/examples/tests/rackunit-typechecking)
+
+(check-type (color "green") : Producer)
+(check-type (color "blue" #:length 2) : (Producer 2))
+(check-not-type (color "blue" #:length 2) : (Producer 3))
+(check-type (clip "vid.mp4" #:start 100 #:end 103) : (Producer (+ 1 (- 103 100))))
+(check-type (clip "vid.mp4" #:start 100 #:end 103) : (Producer 4))
+(check-not-type (clip "vid.mp4" #:start 100 #:end 103) : (Producer 5))
+
+(check-type
+ (playlist
+  (color "green")
+  (color "blue" #:length 2)
+  (clip "vid.mp4" #:start 100 #:end 103))
+ : Producer)
 (color "green")
 (color "blue" #:length 2)
 (clip "vid.mp4" #:start 100 #:end 103)
--- examples/paper1.rkt
+++ examples/paper1.rkt
@@ -1,17 +1,28 @@
-#lang video
-(require video/core)
-(color "green" #:length 1)
+#lang typed/video
+(require turnstile/examples/tests/rackunit-typechecking)
 
+(check-type (color "green" #:length 1) : (Producer 1))
+
+(check-type
+ (multitrack
+  (image "circ.png" #:length (producer-length blue-clip))
+  (composite-transition 0 0 3/4 3/4)
+  blue-clip)
+ : (Producer 8))
+
+(check-type (fade-transition #:length 2) : (Transition 2))
+
+(check-type (clip "vid.mp4" #:length 3) : (Producer 3))
+
+(color "green" #:length 1)
 (multitrack
- (image "circ.png" #:length (blue-length))
+ (image "circ.png" #:length (producer-length blue-clip))
  (composite-transition 0 0 3/4 3/4)
  blue-clip)
 
 ; (swipe-transition #:direction 'up #:length 2) ; TODO
-;(fade-transition #:length 2)
+(fade-transition #:length 2)
 
 (clip "vid.mp4" #:length 3)
 
-;(define blue-clip (color "blue" #:length 8))
-(define blue-clip (color "blue"))
-(define (blue-length) 8)
+(define blue-clip (color "blue" #:length 8))
