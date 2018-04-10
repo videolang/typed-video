8b6dec62f217463d55ab8612c85d812184059edb
Stephen Chang <stchang@ccs.neu.edu>
2018-04-10 17:11:26 -0400

    cut down my-typed-lang (but see michaels example instead)

--- examples/my-typed-lang.rkt
+++ examples/my-typed-lang.rkt
@@ -1,13 +1,15 @@
 #lang racket
 (require (for-syntax syntax/parse racket/syntax syntax/stx))
 (require (prefix-in v: racket))
-(provide #%module-begin #%app provide
+(provide #%module-begin #%app provide #%datum
          (rename-out [my-define define]
-                     [typed-datum #%datum]))
+                     #;[typed-datum #%datum]))
 
 (begin-for-syntax
 
-  (define (new-eval t)
+  (define (le e) (local-expand e 'expression null))
+  (define (to e) (syntax-property e ':))
+  #;(define (new-eval t)
     (syntax-parse (local-expand t 'expression null)
       [t #:do[(printf "EVALING: ~a\n" (syntax->datum #'t))
               (printf "with ty: ~a\n" (syntax-property #'t ':))]
@@ -16,20 +18,28 @@
        #:do[(define k-as-ty (syntax-property #'t+ ':))]
        #:when k-as-ty
        k-as-ty]
-      [((~literal #%plain-app) (~literal list) n)
+      #;[((~literal #%plain-app) (~literal list) n)
        (local-expand #`(list #,(new-eval #'n)) 'expression null)]))
   )
 
 (define-syntax my-define
   (syntax-parser
     [(_ x e)
-     #:with ty #`(list #,(local-expand (new-eval #'e) 'expression null))
-     #'(define-syntax x (new-eval #'ty))
-     ;; #'(define-syntax x
-     ;;     (local-expand #`(list #,(new-eval #'ty)) 'expression null))
+     ;     #:with ty #`(list #,(le (syntax-property (le #'e) ':)))
+     ;     #:with e+ (le #'e)
+     #:with e+ (syntax-property #'(v:#%datum . e) ': #'e)
+     #:with ty #`(list e+)
+;     #:with ty (le #'e)
+;     #'(define-syntax x (new-eval #'ty))
+     #'(define-syntax x
+         (syntax-parse (le #'ty)
+           [x
+            #:do[(displayln #'x)]
+            #:when #f #'(void)]
+           [(_ _ n) (le #`(list #,(to (le #'n))))]))
      ]))
    
-(define-syntax typed-datum
+#;(define-syntax typed-datum
   (syntax-parser
-    [(_ . n:integer)  ; use singleton types
+    [(_ . n)  ; use singleton types
      (syntax-property #'(v:#%datum . n) ': #'n)]))
